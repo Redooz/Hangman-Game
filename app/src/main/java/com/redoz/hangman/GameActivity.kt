bd126373e9de4e016c3d46565f3c79345c2d6895
Nicolás <yohannicolas2003@gmail.com>
2023-03-17 20:29:31 -0500

    Add method to add the letters space

--- app/src/main/java/com/redoz/hangman/GameActivity.kt
+++ app/src/main/java/com/redoz/hangman/GameActivity.kt
@@ -1,11 +1,51 @@
 package com.redoz.hangman
 
+import android.content.Context
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.redoz.hangman.databinding.ActivityGameBinding
 
 class GameActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGameBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
+
+        binding = ActivityGameBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
+
+    private fun loadLettersSpace(word: String) {
+        for (i in word.indices) {
+            val marginEnd = dpToPx(3.toFloat(), this)
+            val textView = TextView(this).apply {
+                layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                ).apply {
+                    setMargins(0, 0, marginEnd, 0)
+                }
+                layoutParams = this.layoutParams
+                typeface = Typeface.MONOSPACE
+                text = "_"
+                setTextColor(ContextCompat.getColor(context, R.color.black))
+                setTextSize(TypedValue.COMPLEX_UNIT_SP, 34f)
+                setTypeface(null, Typeface.BOLD)
+            }
+
+            binding.wordContainer.addView(textView)
+        }
+    }
+
+    private fun dpToPx(dp: Float, context: Context): Int {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics)
+            .toInt()
+    }
+
 }
