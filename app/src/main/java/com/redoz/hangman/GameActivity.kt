571486c71c6e8433c3411796db2a8d8d521840ad
Nicolás <yohannicolas2003@gmail.com>
2023-03-18 20:32:55 -0500

    User can input letters and if it's found on the word, it will be displayed in the activity

--- app/src/main/java/com/redoz/hangman/GameActivity.kt
+++ app/src/main/java/com/redoz/hangman/GameActivity.kt
@@ -13,6 +13,7 @@ import kotlin.random.Random
 
 class GameActivity : AppCompatActivity() {
     private lateinit var binding: ActivityGameBinding
+    private lateinit var lettersMap: MutableMap<Int, TextView>
     private val handmanImages = listOf(
         R.drawable.hangman1,
         R.drawable.hangman2,
@@ -25,7 +26,13 @@ class GameActivity : AppCompatActivity() {
         R.drawable.hangman9,
         R.drawable.hangman10
     )
-    private var changeImageCounter = -1
+    private var changeImageCounter = 0
+    val sports = listOf(
+        "swimming", "cycling", "tennis", "boxing", "shooting", "judo", "golf",
+        "basketball", "football", "volleyball", "baseball", "triathalon",
+        "snowboarding", "hockey", "gymnastics", "bowling", "athletics",
+        "weightlifting", "archery", "badminton", "diving", "cricket"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +40,39 @@ class GameActivity : AppCompatActivity() {
         binding = ActivityGameBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+        lettersMap = mutableMapOf()
 
+        val word = sports[Random.nextInt(0,sports.size)]
+
+        loadLettersAndSpaces(word)
+
+        binding.sendBtn.setOnClickListener {
+            val letter = binding.letterEditTxt.text.toString().trim()
+            if (letter.isNotEmpty()) {
+                findLetterOnWord(letter[0], word)
+                binding.letterEditTxt.text.clear()
+            }
+        }
+    }
+
+    private fun findLetterOnWord(letter: Char, word: String) {
+        var found = false
+        for (i in word.indices) {
+            if (word[i] == letter) {
+                loadFoundLetter(letter, i)
+                found = true
+            }
+        }
+
+        if (!found) {
+            changeHandmanImage()
+            changeImageCounter++
+        }
+    }
+
+    private fun loadFoundLetter(letter: Char, index: Int) {
+        val textView = lettersMap[index]
+        textView?.text = letter.toString()
     }
 
     private fun loadLettersAndSpaces(word: String) {
@@ -41,16 +80,17 @@ class GameActivity : AppCompatActivity() {
         var maxHelpers = word.length/3
         var character = "_"
 
-        for (char in word) {
+        for (i in word.indices) {
             val marginEnd = dpToPx(3.toFloat(), this)
-            var willHelp = Random.nextBoolean()
+            val willHelp = Random.nextBoolean()
 
             if (willHelp && counter < maxHelpers) {
-                character = char.toString()
+                character = word[i].toString()
                 counter++
             }
 
             val textView = TextView(this).apply {
+                id = i
                 layoutParams = LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.WRAP_CONTENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT
@@ -66,13 +106,14 @@ class GameActivity : AppCompatActivity() {
             }
 
             binding.wordContainer.addView(textView)
+            lettersMap[textView.id] = textView
             character = "_"
         }
     }
 
     private fun changeHandmanImage() {
         if (changeImageCounter >= handmanImages.size) {
-            changeImageCounter = -1
+            changeImageCounter = 0
         }
 
         binding.imgHandman.setImageResource(handmanImages[changeImageCounter])
